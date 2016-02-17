97188f860005c16d7e8e7f42368ec84a6550ccfe
Mikkel <Mikkel@10.16.166.160>
2016-02-17 14:13:21 +0100

    Test af drone klasse

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,8 @@ package main;
 import control.DroneControl;
 import de.yadrone.base.ARDrone;
 import de.yadrone.base.IARDrone;
+import de.yadrone.base.configuration.ConfigurationManager;
+import de.yadrone.base.exception.IExceptionListener;
 
 /**
  *
@@ -15,11 +17,34 @@ import de.yadrone.base.IARDrone;
  */
 public class Main {
 
-    static public void main(String[] args) {
-        IARDrone drone = new ARDrone();
-        DroneControl control = new DroneControl(drone);
-        control.run();
+	static public void main(String[] args) {
 
-    }
+		IARDrone drone = null;
+		try {
+			drone = new ARDrone();
+			System.out.println("Starting Drone");
+			drone.start();
+		} catch (Exception exc) {
+			System.err.println(exc.getMessage());
+			exc.printStackTrace();
+		}
+		
+//		IExceptionListener il = new ExceptionListener();
+//		drone.addExceptionListener(il);
 
+		ConfigurationManager cm = drone.getConfigurationManager();
+
+		System.out.println("Drone connected: " + cm.isConnected());
+
+		System.out.println("Starting Drone Control");
+		DroneControl control = new DroneControl(drone);
+		control.run();
+
+		System.out.println("Shutting down");
+		cm.close();
+		if (drone != null) {
+			drone.stop();
+		}
+		System.exit(0);
+	}
 }
