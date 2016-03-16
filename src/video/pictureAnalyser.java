3ed79325c3ffc11c5b597734fa4dcb78516ec740
Simon <simon_petersen1994@hotmail.com>
2016-03-16 10:45:42 +0100

    Fjernet import.

--- src/video/pictureAnalyser.java
+++ src/video/pictureAnalyser.java
@@ -25,7 +25,6 @@ import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
-import org.opencv.videoio.VideoCapture;
 
 /**
  *
