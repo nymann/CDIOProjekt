4f152c9879b191073bf9af84f66ee242d8123515
Nymann <s144833@student.dtu.dk>
2016-03-16 11:00:38 +0100

    Now prints some of the listener values.

--- src/main/Main.java
+++ src/main/Main.java
@@ -116,7 +116,8 @@ public class Main {
 			BufferedImage image = vr.getImage();
 			//System.out.println("new image ready");
 			graphics.drawImage(image, 0, 0, window);
-			System.out.println("Battery: " + battery.level + ". Velocity, X: " +
+			System.out.println("Battery: " + battery.level + "%. Velocity, X:" +
+					" " +
 					"" + velocity.vx + ", Y: " + velocity.vy + ", Z: " +
 					velocity.vz + ".");
 		}
