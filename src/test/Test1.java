7c348506a4c28791c8bd5758a46675abb5f274ee
nehfi <kim@aptget.dk>
2016-03-30 11:38:11 +0200

    lavet testklasse til at flyve rundt. utestet

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -1,5 +1,51 @@
 package test;
 
+import de.yadrone.base.ARDrone;
+import de.yadrone.base.IARDrone;
+import de.yadrone.base.configuration.ConfigurationManager;
+import de.yadrone.base.navdata.NavDataManager;
+
+/**
+ * 
+ * @author Kim & Sandie
+ * 
+ * Testklasse til at prøve at navigere rundt i et afgrænset specifikt rum.
+ * 
+ */
+
 public class Test1 {
+
+	static private IARDrone drone = null;
+
+	static public void main(String[] args) {
+
+		// connecting to drone
+		try {
+			drone = new ARDrone();
+			System.out.println("Starting Drone");
+			drone.start();
+		} catch (Exception exc) {
+			System.err.println(exc.getMessage());
+			exc.printStackTrace();
+		}
+
+		// getting managers
+		ConfigurationManager cm = drone.getConfigurationManager();
+		NavDataManager nm = drone.getNavDataManager();
 		
+		drone.setMaxAltitude(3000);
+		
+		drone.start();
+		drone.hover();
+		drone.forward();
+		drone.stop();
+		drone.spinLeft();
+		drone.forward();
+		drone.landing();
+		drone.stop();
+		
+		System.exit(0);
+		
+	}
+
 }
