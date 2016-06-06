1b653fc6d10d44f31a7a10c3d44682643654f3df
Blumen <rasmus_blumensaadt@hotmail.com>
2016-06-06 11:52:29 +0200

    getOrientation oprettet

--- src/modeling/FlowVector.java
+++ src/modeling/FlowVector.java
@@ -8,16 +8,25 @@ import org.opencv.core.Point;
  *
  */
 public class FlowVector {
-	
+
 	public Point p1, p2;
-	
+
 	public FlowVector(Point p1, Point p2) {
 		this.p1 = p1;
 		this.p2 = p2;
 	}
-	
+
 	public double getLength() {
-		return Math.sqrt(Math.pow(p2.x-p1.x, 2)+Math.pow(p2.y-p1.y, 2));
+		return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
 	}
 
+	public int getOrientation(Point centerpoint) {
+		int orientation = 0;
+		if (Math.abs(this.p2.x - 500) < Math.abs(this.p1.x - 500)
+				&& Math.abs(this.p2.y - 500) < Math.abs(this.p1.y - 500))
+			orientation += 1;
+		else
+			orientation -= 1;
+		return orientation;
+	}
 }
