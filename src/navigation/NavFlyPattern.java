eece441fe456b3ab0703f478ca694743a2e967ea
nehfi <kim@aptget.dk>
2016-06-06 14:32:50 +0200

    navigation

--- src/navigation/NavFlyPattern.java
+++ src/navigation/NavFlyPattern.java
@@ -1,5 +1,10 @@
 package navigation;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import modeling.QRPoint;
+
 /**
 *
 * @author Kim
@@ -14,17 +19,34 @@ public class NavFlyPattern {
 	 */
 
 	NavigationControl nc = new NavigationControl();
-	
+	List<NavSpot> spots = new ArrayList<>();
 	
 	private void startSpot(){
+		/*
+		 * make sure currentposition matches startSpot
+		 */
 
 	}
 
 	private void endSpot(){
+		/*
+		 * make sure currentposition matches endSpot
+		 * if not, calculate if within acceptable
+		 */
 
 	}
 
 	private void lane(){
-
+		/*
+		 * using OF, fly between one start and end
+		 * once every 1 meter or something take picture, get this analyzed for cubes
+		 * needs plan for flying around/over boxes
+		 */
+ 
+	}
+	
+	public void fillStartEndSpots(){
+		//x,y,type; 1=startSpot | 2=endSpot
+		spots.add(new NavSpot(11, 22, 1));
 	}
 }
