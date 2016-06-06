dcd166873e9f014603590cc0853edaf2b3c32c4b
Nymann <s144833@student.dtu.dk>
2016-06-06 10:53:14 +0200

    QRCode cleanup.

--- src/gui/VideoPanel.java
+++ src/gui/VideoPanel.java
@@ -29,11 +29,7 @@ public class VideoPanel extends JPanel implements ImageListener{
 		this.image = bi;
 		this.setSize(bi.getWidth(), bi.getHeight());
 
-		// qrResult[0] is the decoded QR code, or an error message.
-		// qrResult[1] is the pixel coordinate of the QR code center as a
-		// string "300.00, 150.00".
-		String[] qrResult;
-		qrResult = QRWallMarks.GetQRCode.readQRCode(bi);
+		qrCodeResult();
 	}
 	
 	@Override
@@ -45,5 +41,23 @@ public class VideoPanel extends JPanel implements ImageListener{
 			g.fillRect(0, 0, this.getWidth(), this.getHeight());
 		}
 	}
-	
+
+	private void qrCodeResult() {
+		// qrResult[0] is the decoded QR code, or an error message
+		// qrResult[1] is the pixel coordinate of the QR code center as a
+		// string "300.00, 150.00".
+		String[] qrResult;
+		qrResult = QRWallMarks.GetQRCode.readQRCode(this.image);
+		float xMiddleCoordinate = Float.valueOf(qrResult[1].substring(0,
+				qrResult[1].indexOf(",")));
+		float yMiddleCoordinate = Float.valueOf(qrResult[1].substring
+				(qrResult[1].indexOf(",") + 1));
+		String qrDecodeMessage = qrResult[0];
+
+		System.out.println("Decodemessage: " + qrDecodeMessage + ". " +
+				xMiddleCoordinate + ", " +
+				"" +
+				yMiddleCoordinate +
+				".");
+	}
 }
