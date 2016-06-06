195ed53e40c7f1ebfd00d73655325e3a6438477e
nehfi <kim@aptget.dk>
2016-06-06 14:04:09 +0200

    Har oprettet klasserne til at begynde og navigere rundt på banen

---
+++ src/navigation/NavFlyPattern.java
@@ -0,0 +1,30 @@
+package navigation;
+
+/**
+*
+* @author Kim
+*/
+
+public class NavFlyPattern {
+	
+	/*
+	 * assume at startspot
+	 * fly lane
+	 * reach endspot
+	 */
+
+	NavigationControl nc = new NavigationControl();
+	
+	
+	private void startSpot(){
+
+	}
+
+	private void endSpot(){
+
+	}
+
+	private void lane(){
+
+	}
+}
---
+++ src/navigation/NavigationControl.java
@@ -0,0 +1,32 @@
+package navigation;
+
+/**
+ *
+ * @author Kim
+ */
+
+public class NavigationControl {
+	/*
+	 * NavFindPosition
+	 * NavFlyingDrone(currentPos, startSpot1)
+	 * NavFlyPattern(1)
+	 * NavFindPosition
+	 * NavFlyPattern(2)
+	 * .
+	 * .
+	 * PresentResults()
+	 */
+
+	NavFindPosition findPos = new NavFindPosition();
+	NavFlyPattern flyPat = new NavFlyPattern();
+	NavFlyingDrone flyingDrone = new NavFlyingDrone();
+	
+	private void runNav (){
+		
+		
+	}
+	
+	private void presentResults(){
+
+	}
+}
