9b897cdb2919b2c14333101d1d5370e1966162a1
Mikkel <Mikkel@10.16.166.160>
2016-06-10 13:28:51 +0200

    Man kan tilføje og fjerne listeners til attitude listener

--- src/listeners/Attitude.java
+++ src/listeners/Attitude.java
@@ -1,7 +1,10 @@
 package listeners;
 
 import de.yadrone.base.navdata.AttitudeListener;
+import de.yadrone.base.video.ImageListener;
 import gui.ListenerValuePanel;
+import java.util.ArrayList;
+import java.util.List;
 import modeling.Angle3D;
 import modeling.MainModel;
 
@@ -10,8 +13,10 @@ import modeling.MainModel;
  */
 public class Attitude implements AttitudeListener {
     MainModel model;
+	List<AttitudeListener> listeners;
 
     public Attitude(MainModel model){
+		this.listeners = new ArrayList<>();
         this.model = model;
     }
 
@@ -21,16 +26,48 @@ public class Attitude implements AttitudeListener {
 		attitude.setPitch(pitch*Math.PI/180000);
 		attitude.setRoll(roll*Math.PI/180000);
 		model.setDroneAttitude(attitude);
+		for (AttitudeListener listener : listeners) {
+			new Thread() {
+				@Override
+				public void run() {
+					listener.attitudeUpdated(pitch,roll);
+				}
+			}.start();
+		}
 	}
 
     @Override
     public void attitudeUpdated(float pitch, float roll, float yaw) {
 		Angle3D attitude = new Angle3D(pitch*Math.PI/180000, roll*Math.PI/180000, yaw*Math.PI/180000);
 		model.setDroneAttitude(attitude);
+		for (AttitudeListener listener : listeners) {
+			new Thread() {
+				@Override
+				public void run() {
+					listener.attitudeUpdated(pitch,roll,yaw);
+				}
+			}.start();
+		}
     }
 
     @Override
     public void windCompensation(float pitch, float roll) {
+		for (AttitudeListener listener : listeners) {
+			new Thread() {
+				@Override
+				public void run() {
+					listener.windCompensation(pitch, roll);
+				}
+			}.start();
+		}
     }
+	
+	public void addListener(AttitudeListener listener){
+		listeners.add(listener);
+	}
+	
+	public void removeListener(AttitudeListener listener){
+		listeners.remove(listener);
+	}
 
 }
