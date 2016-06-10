f79c44644c8256ff9ceed960b59fb3d4f78ae75c
Mikkel <Mikkel@10.16.166.160>
2016-06-10 12:54:56 +0200

    Attitude listener ændre model

--- src/listeners/Attitude.java
+++ src/listeners/Attitude.java
@@ -2,43 +2,35 @@ package listeners;
 
 import de.yadrone.base.navdata.AttitudeListener;
 import gui.ListenerValuePanel;
+import modeling.Angle3D;
+import modeling.MainModel;
 
 /**
  * @author Simon
  */
 public class Attitude implements AttitudeListener {
-    ListenerValuePanel listenerValuePanel;
-    float pitch, roll, yaw;
+    MainModel model;
 
-    public Attitude(ListenerValuePanel listenerValuePanel){
-        this.listenerValuePanel = listenerValuePanel;
+    public Attitude(MainModel model){
+        this.model = model;
     }
 
     @Override
     public void attitudeUpdated(float pitch, float roll) {
-        this.pitch = pitch;
-        this.roll = roll;
-        System.out.println("Attitude Updated: Pitch: " + pitch + " Roll: " + roll);
-        listenerValuePanel.appendText("Attitude Updated: Pitch: " + pitch +
-                " Roll: " + roll);
-    }
+		Angle3D attitude = model.getDroneAttitude();
+		attitude.setPitch(pitch*Math.PI/180000);
+		attitude.setRoll(roll*Math.PI/180000);
+		model.setDroneAttitude(attitude);
+	}
 
     @Override
     public void attitudeUpdated(float pitch, float roll, float yaw) {
-        this.pitch = pitch;
-        this.roll = roll;
-        this.yaw = yaw;
-        //System.out.println("Attitude Updated: Pitch: "+pitch+" Roll: " +
-        //""+roll+" Yaw: "+yaw);
-        listenerValuePanel.appendText("Attitude Updated: Pitch: " + pitch +
-                " Roll: " + roll + " Yaw: " + yaw);
+		Angle3D attitude = new Angle3D(pitch*Math.PI/180000, roll*Math.PI/180000, yaw*Math.PI/180000);
+		model.setDroneAttitude(attitude);
     }
 
     @Override
     public void windCompensation(float pitch, float roll) {
-        this.pitch = pitch;
-        this.roll = roll;
-        System.out.println("Wind Compensation: Pitch: " + pitch + " Roll: " + roll);
     }
 
 }
