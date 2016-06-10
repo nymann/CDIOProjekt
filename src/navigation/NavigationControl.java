dc85a6b202e9f1c6cae814a666a510103b16b255
nehfi <kim@aptget.dk>
2016-06-10 12:11:47 +0200

    nav update

--- src/navigation/NavigationControl.java
+++ src/navigation/NavigationControl.java
@@ -6,6 +6,7 @@ import de.yadrone.base.IARDrone;
 import de.yadrone.base.command.CommandManager;
 import de.yadrone.base.video.ImageListener;
 import de.yadrone.base.video.VideoManager;
+import modeling.MainModel;
 import video.VideoReader;
 
 /**
@@ -17,6 +18,7 @@ public class NavigationControl {
  
 	private VideoManager vm;
 	private CommandManager cm;
+	private MainModel mm;
 	private NavFlyPattern flyPat;
 	private NavFindPosition findPos;
 	private video.VideoReader vr;
@@ -30,6 +32,8 @@ public class NavigationControl {
 		cm = drone.getCommandManager();
 		vr = new VideoReader(vm, cm);
 		
+		mm = NavFlyPattern(vr, , mm);
+		
 		runNav();
 		presentResults();
 	}
@@ -42,7 +46,7 @@ public class NavigationControl {
 		try {
 			for (int i=0; i<14; i++) {
 				flyPat.flyToSpot(xPos, yPos, i);
-				if(flyPat.atSpot(0)) flyPat.flyLane(i, i+1);
+				if(flyPat.atSpot(xPos, yPos, i)) flyPat.flyLane(i, i+1);
 				else flyPat.flyToSpot(xPos, xPos, i);
 			}
 		} catch (Exception e) {
