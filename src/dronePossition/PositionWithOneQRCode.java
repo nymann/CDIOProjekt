fc63b9e11d4fdb580c5e174d9620ce20f3736960
Sandie <sandieAkkermans@hotmail.com>
2016-06-16 14:09:04 +0200

    En QR kode og to QR koder, detekt ca. hvor vi er.

--- src/dronePossition/PositionWithOneQRCode.java
+++ src/dronePossition/PositionWithOneQRCode.java
@@ -5,7 +5,7 @@ import javafx.geometry.Point2D;
 public class PositionWithOneQRCode {
 	
 	double x, y, x1, x2, y1, y2;
-	double d = 21.2, p, k1, k2, alpha, beta, fullLength;
+	double d = 21.2, p, k1, k2, alpha, beta, fullLength, r1, r2;
 	
 	public double findLenght() {
 		k1 = d/p;
@@ -26,25 +26,38 @@ public class PositionWithOneQRCode {
 		return fullLength;
 	}
 	
-	public double Length() {
+	public double LengthOneQR() {
 		findLenght();
 		findAngel();
 		return LengthBetweenQRAndDrone();
 	}
 	
-	public Point2D findPosition() {
-		double firstLength = Length();
-		//Drone skal have læst en ny QR kode imellem disse kald.
-		double secondLength = Length();
+	public Point2D findPositionTwoQR() {
 		
-		double c1 = Math.pow((x-x1), 2)+Math.pow((y-y1), 2)-Math.pow(firstLength,2);
-		double c2 = Math.pow((x-x2), 2)+Math.pow((y-y2), 2)-Math.pow(secondLength,2);
+		double f1, f2, yres1, yres2, xres1, xres2, dist1, dist2;
+		k1 = -0.5*(2*y1-2*y2)/(x1-x2);
+		k2 = -0.5*(r1*r1-r2*r2-x1*x1+x2*x2-y1*y1+y2*y2)/(x1-x2);
 		
-		x = Math.pow((-Math.pow((y-y1), 2) + firstLength*firstLength + Math.pow((y-y2),2) - secondLength*secondLength - x1*x1 + x2*x2)/(-2*x1+2*x2)-x1,2)+Math.pow((y-y1),2);
-		// y = Math.sqrt(firstLength*firstLength-Math.pow((x-x1), 2)) + y1;
+		f1 = Math.sqrt(k1*k1*r1*r1-k1*k1*y1*y1-2*k1*k2*y1+2*k1*x1*y1-k2*k2+2*k2*x1+r1*r1-x1*x1);
+		f2 = k1*k1+1;
+		yres1 = ((-k1*k2+k1*x1+f1)+y1)/f2;
+		yres2 = -((k1*k2-k1*x1+f1)-y1)/f2;
 		
-		Point2D dronePositioning = new Point2D(x, y);
+		xres1 = yres1*k1+k2;
+		xres2 = yres2*k1+k2;
 		
+		Point2D dronePositioning = new Point2D(0, 0);
+		
+		if(xres1 > 0 && xres1 < 926) {
+			if(yres1 > 0 && yres1 < 1060) {
+				dronePositioning.add(xres1, yres1);
+				return dronePositioning;
+			}
+		}
+		else {
+			dronePositioning.add(xres2, yres2);
+			return dronePositioning;
+		}
 		return dronePositioning;
 	}
 	
