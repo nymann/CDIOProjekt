14e434f3481e44290096bf67b4c88459a211d1dc
JoachimØstergaard <jokses@gmail.com>
2016-06-16 10:51:33 +0200

    clean up in picture analyse adn view

--- src/video/PictureView.java
+++ src/video/PictureView.java
@@ -101,14 +101,4 @@ public class PictureView {
 		return out;
 	}
 
-	public static Image mat2Image(Mat frame) {
-		// create a temporary buffer
-		MatOfByte buffer = new MatOfByte();
-		// encode the frame in the buffer, according to the PNG format
-		Imgcodecs.imencode(".png", frame, buffer);
-		// build and return an Image created from the image encoded in the
-		// buffer
-		return new Image(new ByteArrayInputStream(buffer.toArray()));
-	}
-
 }
