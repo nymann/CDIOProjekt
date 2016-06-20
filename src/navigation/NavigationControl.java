bdccf524cf1924f538e3201135ddba265d12a741
Nymann <s144833@student.dtu.dk>
2016-06-20 09:41:09 +0200

    formatted naviagtion control

--- src/navigation/NavigationControl.java
+++ src/navigation/NavigationControl.java
@@ -1,69 +1,62 @@
 package navigation;
 
-import java.awt.image.BufferedImage;
-
-import javafx.geometry.Point3D;
 import de.yadrone.base.IARDrone;
 import de.yadrone.base.command.CommandManager;
-import de.yadrone.base.video.ImageListener;
 import de.yadrone.base.video.VideoManager;
+import javafx.geometry.Point3D;
 import modeling.MainModel;
-import video.VideoReader;
+
+import java.awt.image.BufferedImage;
 
 /**
- *
  * @author Kim
  */
 
 public class NavigationControl {
- 
-	private VideoManager vm;
-	private CommandManager cm;
-	private MainModel mm;
-	private NavFlyPattern flyPat;
-	private NavFindPosition findPos;
-	private ImageDataListener idl;
-	private IARDrone drone;
-	private BufferedImage buffI;
-	private Point3D pos3d;
 
-	NavigationControl(ImageDataListener idl){
-		vm = drone.getVideoManager();
-		cm = drone.getCommandManager();
-		cm.setControlAck(true);
-		findPos = new NavFindPosition(idl, drone);
-		flyPat = new NavFlyPattern(idl, drone);
-		
-		runNav();
-		presentResults();
-	}
+    private VideoManager vm;
+    private CommandManager cm;
+    private MainModel mm;
+    private NavFlyPattern flyPat;
+    private NavFindPosition findPos;
+    private ImageDataListener idl;
+    private IARDrone drone;
+    private BufferedImage buffI;
+    private Point3D pos3d;
 
-	private void runNav(){
-		
-		pos3d = mm.getDronePosition();
-		
-		double xPos = pos3d.getX();
-		double yPos = pos3d.getY();
+    NavigationControl(ImageDataListener idl) {
+        vm = drone.getVideoManager();
+        cm = drone.getCommandManager();
+        cm.setControlAck(true);
+        findPos = new NavFindPosition(idl, drone);
+        flyPat = new NavFlyPattern(idl, drone);
 
-		try {
-			for (int i=0; i<14; i++) {
-				flyPat.flyToSpot(i);
-				if(flyPat.atSpot(i)) flyPat.flyLane(i, i+1);
-				else flyPat.flyToSpot(i);
-			}
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		
-		
-		
-		
-	}
+        runNav();
+        presentResults();
+    }
 
-	private void presentResults(){
-		/*
+    private void runNav() {
+
+        pos3d = MainModel.getDronePosition();
+
+        double xPos = pos3d.getX();
+        double yPos = pos3d.getY();
+
+        try {
+            for (int i = 0; i < 14; i++) {
+                flyPat.flyToSpot(i);
+                if (flyPat.atSpot(i)) flyPat.flyLane(i, i + 1);
+                else flyPat.flyToSpot(i);
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    private void presentResults() {
+        /*
 		 * Ska gemme resultaterne vdr cubes i en fil
 		 */
 
-	}
+    }
 }
