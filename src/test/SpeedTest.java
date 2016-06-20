fee37368a4ebb8bede21fc62d060eec03a1a495d
Nymann <s144833@student.dtu.dk>
2016-06-20 10:48:18 +0200

    SpeedTest added

--- src/test/SpeedTest.java
+++ src/test/SpeedTest.java
@@ -2,13 +2,15 @@ package test;
 
 /**
  * Created by Nymann on 20-06-2016.
- *  1. find position
- *  2. find nearest point
+ *  1. find position - X
+ *  2. find nearest point - Rasmus lavet. (MainModel)
  *  3. spinLeft until you are facing the point. (method in NavFlyPattern)
  *  4. move forward (move(20, 0, 0, 0)).doFor(X).
  *  5. after X seconds findPosition, if we are close to the point, then remove that point from the arrayList and go to step 1.
  */
 public class SpeedTest {
+
+
     public SpeedTest() {
 
     }
